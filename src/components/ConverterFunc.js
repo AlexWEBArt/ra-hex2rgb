1b1add13f0a27183283227ac977f0fba2ba73f9a
AlexWebArt <elusiv@mail.ru>
2023-04-28 18:50:24 +0600

    update patern 28.04.23

--- src/components/ConverterFunc.js
+++ src/components/ConverterFunc.js
@@ -8,20 +8,25 @@ export default function Converter() {
         e.peventDefault();
     }
     const handleChange = (e) => {
-        const { value } = e.target
+        let { value } = e.target
+        if (value[0] !== '#') {
+            value = '#' + value;
+          }
         setHex(prevHex => ({...prevHex, value}))
     }
     const hexToRGBArray = (hex) => {
         if (hex.length === 7) {
-            return 'rgb(' + hex.match(/[A-Za-z0-9]{2}/g).map((item, index) => index === 0 ? parseInt(item, 16) : ' ' + parseInt(item, 16)) + ')'
+            if (!hex.match('[A-Fa-f0-9]{6}')) {
+                return 'Ошибка!'
+            }
+            return 'rgb(' + hex.match(/[A-Fa-f0-9]{2}/g).map((item, index) => index === 0 ? parseInt(item, 16) : ' ' + parseInt(item, 16)) + ')'
         }
-        return 'Ошибка!'
     };
     const backColor = hexToRGBArray(hex.value) === 'Ошибка!' ? '#fa5741' : hexToRGBArray(hex.value)
     return (
         <div className="background-container" style={ {backgroundColor: backColor} }>
             <form className="form-hex" onSubmit={handleSubmit}>
-                <input className="input-hex" name='hex' value={hex.value} onChange={handleChange}>
+                <input className="input-hex" name='hex' maxLength='7' value={hex.value} onChange={handleChange}>
 
                 </input>
                 <span className="convert-rgb">
